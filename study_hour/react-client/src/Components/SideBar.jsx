8145183d880d4256755edf349f3b25a63cb9a952
Elliot <elliot@pourmand.com>
2018-09-27 16:26:46 -0700

    cleaning it out

--- study_hour/react-client/src/Components/SideBar.jsx
+++ study_hour/react-client/src/Components/SideBar.jsx
@@ -2,16 +2,11 @@ import React, {Component} from 'react';
 import { URLProvider } from 'react-url';
 import home from '../images/home.svg'
 import profile from '../images/profile.svg'
-import chess from '../images/chess.svg'
 import '../styles/style.css'
 import Img from 'react-image';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import { Link } from 'react-router-dom'
-import resume from '../images/resume.svg'
-import Button from 'material-ui/FlatButton'
-import Chess from 'react-chess';
-import chat from '../images/chat.svg'
 
 const style = {
     display: 'inline-block',
@@ -31,11 +26,7 @@ class SideBar extends Component {
     render() {
         return (
             <div>
-                {/*<Button onClick={this.toggleDrawer} zDepth={3} className='button-s'>*/}
-                    {/*<h1>Open Left</h1>*/}
-                {/*</Button>*/}
             <Drawer open={this.state.open}
-                    // onClose={this.toggleDrawer(false)}
                     containerClassName='left-drawer'
                     zDepth={2}>
                 <MenuItem className='menu-item'>
@@ -44,24 +35,8 @@ class SideBar extends Component {
                     </Link>
                 </MenuItem>
                 <MenuItem className='menu-item'>
-
                     <Link to={"About"}>
                         <Img src={profile} className="img-right"/>
-                    </Link>
-                </MenuItem>
-                <MenuItem className='menu-item'>
-                    <Link to={"Chess"}>
-                        <img src={chess} className='img-right'/>
-                    </Link>
-                </MenuItem>
-                <MenuItem className='menu-item'>
-                    <Link to={"Resume"}>
-                        <img src={resume} className='img-right'/>
-                    </Link>
-                </MenuItem>
-                <MenuItem className='menu-item'>
-                    <Link to={"Chat"}>
-                        <img src={chat} className='img-right'/>
                     </Link>
                 </MenuItem>
             </Drawer>
--- study_hour/server/index.js
+++ study_hour/server/index.js
@@ -6,7 +6,6 @@ const app = express();
 const pg = require('pg');
 const config = require('../config.js');
 
-var PythonShell = require('python-shell');
 
 db = config.database;
 var pgClient = new pg.Client(db);
@@ -27,33 +26,5 @@ app.get('/Home', (req, res) => {
 });
 app.get('/About', (req, res) => {
     res.sendFile(path.resolve(`${__dirname}/../react-client/dist/index.html`));
-});
-app.get('/Chess', (req, res) => {
-    res.sendFile(path.resolve(`${__dirname}/../react-client/dist/index.html`));
-});
-app.get('/Resume', (req, res) => {
-    res.sendFile(path.resolve(`${__dirname}/../react-client/dist/index.html`));
-});
-
-app.get('/Chat', (req, res) => {
-    res.sendFile(path.resolve(`${__dirname}/../react-client/dist/index.html`));
-});
-
-app.get('/GetComments', (req, res) => {
-    pgClient.query("TABLE comments").then(result => {
-            res.json(result.rows)
-        }
-    );
-});
-
-app.post('/PostComment', function (req, res, next) {
-    const comment_post = req.body.params;
-    pgClient.query('INSERT INTO comments (poster_name, comment) VALUES ($1, $2);', [comment_post.poster_name, comment_post.comment], function (err, result) {
-        if (err) {
-            // pass the error to the express error handler
-            return next(err)
-        }
-        res.send("Success Posting Comment")
-    })
 });
 
