c9a60818d69566eaabf57e0935c21136e56b23e3
Rogerch99 <rogerch99@gmail.com>
2018-10-29 23:01:10 -0700

    finished adding routes ;

--- study_hour/server/index.js
+++ study_hour/server/index.js
@@ -42,13 +42,29 @@ app.get('/Login', (req, res) => {
 // Get all the comments for a location
 // Returned comment should have all comment data including user_id
 app.post('/api/Location/Comments', function (req, res, next) {
-
+    
+    console.log("incomming request", req.body.location_id);
+    pgClient.query('SELECT * FROM Comments t WHERE t.location_id= $1 ', [req.body.location], function (err, result) {
+        if (err) {
+            return next(err)
+        }
+        console.log("db result: ", result.rows);
+        res.send({dbresponse: result.rows})
+    });
 
 });
 
 // Get all comments from a given user
 // not that high priority
 app.post('/api/User/Comments', function (req, res, next) {
+    console.log("incomming request", req.body.location);
+    pgClient.query('SELECT * FROM Comments t WHERE t.user_id= $1 ', [req.body.location], function (err, result) {
+        if (err) {
+            return next(err)
+        }
+        console.log("db result: ", result.rows);
+        res.send({dbresponse: result.rows})
+    });
 
 
 });
@@ -56,42 +72,38 @@ app.post('/api/User/Comments', function (req, res, next) {
 // Get all data about a user
 app.post('/api/User', function (req, res, next) {
 
+    console.log("incomming request", req.body.user_id);
+    pgClient.query('SELECT * FROM Users t WHERE t.user_id= $1 ', [req.body.user_id], function (err, result) {
+        if (err) {
+            return next(err)
+        }
+        console.log("db result: ", result.rows);
+        res.send({dbresponse: result.rows})
+    });
+
 });
 
 
 // Verify login credentials, return True or False for authentication success
 app.post('/api/Login', function (req, res, next) {
-    // const comment_post = req.body.params;
     console.log(req.body);
-    const query = "SELECT CASE WHEN EXISTS (SELECT * FROM Accounts WHERE username = $1 and password = $2) THEN \'TRUE'\  ELSE \'FALSE'\ END;";
-
-    // pgClient.query(query, [req.body.username, req.body.password], function (err, result) {
-    //     if (err) {
-    //         return next(err)
-    //     }
-    //     console.log("db result: ", result.rows[0].case);
-    //     if (result.rows[0].case == "TRUE") {
-    //
-    //     }
-    //     const token = jwt.sign({ id: user._id }, config.secret, {
-    //         expiresIn: 86400 // expires in 24 hours
-    //     });
-    //     res.send({auth: result.rows[0].case, token: token})
-    // })
-    pgClient.query('SELECT * FROM Accounts where user_name = \'elliot\' and user_password = \'123\'\n', function (err, result) {
+    pgClient.query('SELECT * FROM Users u where u.user_name = $1 and u.password = $2',[req.body.user_name, req.body.password], function (err, result) {
         if (err) {
             return next(err)
         }
         console.log("db result: ", result.rows);
-        // if (result.rows[0].case == "TRUE") {
-        //
-        // }
+        if(result.rows.length === 0) {
+            res.send({auth: false});
+            return;
+        }
+        
         const config = {
             secret: "supersecret"
         };
         const token = jwt.sign({ id: result.rows[0].user_id }, config.secret, {
             expiresIn: 86400 // expires in 24 hours
         });
-        res.send({auth: result.rows[0].case, token: token})
+        res.send({auth: true, token: token})
+        
     });
 });
