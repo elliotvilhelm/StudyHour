859a012a40b373289ad2255b67d690597837bed8
William Vuong <wpvuong@ucsd.edu>
2018-11-28 20:43:34 -0800

    Rewriting Auth.js to 2-space tabs

--- study_hour/react-client/src/actions/auth.js
+++ study_hour/react-client/src/actions/auth.js
@@ -6,36 +6,33 @@ export const UNAUTHENTICATED = 'unauthenticated_user';
 export const AUTHENTICATION_ERROR = 'authentication_error';
 
 
-export function authenticate(username, password) {
-    return (dispatch) =>
-    {
 
-        axios({
-            method: 'post',
-            url: '/api/Login',
-            data: {
-                user_name: username,
-                password: password,
-            },
-            config: {headers: {'Content-Type': 'multipart/form-data'}}
-        })
-            .then(function (response) {
-                if (!response.data.auth) return;
-                localStorage.setItem('user', response.data.token);
-                history.push('/Home');
-                dispatch({type: AUTHENTICATED});
-            })
-            .catch(function (response) {
-                console.log("Error on login db response", response);
-            });
+export function authenticate(username, password){
+  return (dispatch) =>{
+    axios({
+      method: 'post',
+      url: '/api/Login',
+      data:{
+        user_name: username,
+        password: password,
+      },
+      config: {headers: {'Content-Type': 'multipart/form-data'}}
+    
+    }).then((response) => {
+      if(!response.data.auth) return;
+      localStorage.setItem('user', response.data.token);
+      history.push('/Home');
+      dispatch({type: AUTHENTICATED});
 
-    }
+    }).catch((response) => {
+      console.log("Error on login db response", response);
+    })
+  }
 }
 
-export function unauthenticate() {
+export function unauthenticate(){
     localStorage.clear();
-    
-    return {
-        type: UNAUTHENTICATED
-    };
+    return{
+      type: UNAUTHENTICATED
+    }
 }
