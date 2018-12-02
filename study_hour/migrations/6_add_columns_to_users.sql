721b343c4edd18bc98da9f25602e837cb7e9f8fe
leitaku <leitaku@gmail.com>
2018-12-01 19:01:27 -0800

    finished signup with more information

---
+++ study_hour/migrations/6_add_columns_to_users.sql
@@ -0,0 +1,5 @@
+ALTER TABLE users ADD fullname varchar(255) NULL;
+ALTER TABLE users ADD city varchar(255) NULL;
+ALTER TABLE users ADD security_q text NULL;
+ALTER TABLE users ADD security_a text NULL;
+ALTER TABLE users ADD bio text NULL;
--- study_hour/react-client/src/actions/signup_action.js
+++ study_hour/react-client/src/actions/signup_action.js
@@ -2,14 +2,19 @@ import axios from "axios";
 import history from "../history";
 
 
-export function signup(username, password) {
+export function signup(fullname, username, password, city, question, answer, bio) {
     return (dispatch) => {
         axios({
             method: 'post',
             url: '/api/Signup',
             data: {
+                fullname: fullname,
                 user_name: username,
                 password: password,
+                city: city,
+                security_q: question,
+                security_a: answer,
+                bio: bio,
             },
             config: {headers: {'Content-Type': 'multipart/form-data'}}
         })
