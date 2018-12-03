d86b3182c9c3760140c106eaf1fc492464f58057
William Vuong <wpvuong@ucsd.edu>
2018-12-03 03:22:04 -0800

    Partly done with validation

--- study_hour/react-client/src/Components/Login.jsx
+++ study_hour/react-client/src/Components/Login.jsx
@@ -7,7 +7,6 @@ import {TextField, Grid, Typography} from '@material-ui/core';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import {Button} from "@material-ui/core";
 import * as auth_actions from "../actions/auth";
-import * as resetPassword_actions from "../actions/resetPassword_action";
 import { connect } from  "react-redux";
 import { withRouter } from 'react-router-dom';
 
@@ -33,10 +32,12 @@ class Login extends Component {
             username:'',
             password:'',
             submitted:false,
+            usernameInputError:false,
+            userpasswordInputError:false,
+            formError: true,
         };
         this.handleChangeUserName = this.handleChangeUserName.bind(this);
         this.handleChangePassword = this.handleChangePassword.bind(this);
-        this.handleForgotPassword = this.handleForgotPassword.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -48,33 +49,45 @@ class Login extends Component {
         this.setState({password: event.target.value});
     }
 
-    handleForgotPassword() {
-        this.props.dispatch(resetPassword_actions.resetPassword());
-    }
+    handleSubmit(event) {
+        if(this.state.username === ''){
+            this.state.usernameInputError = true; 
+            this.state.formError = true; 
+        }else{
+            this.state.formError = false;
+        }
 
-    handleSubmit() {
-        this.props.dispatch(auth_actions.authenticate(this.state.username, this.state.password));
-        
+        if(this.state.password === ''){
+            this.state.userpasswordInputError = true; 
+            this.state.formError = true;
+        }else{
+            this.state.formError = false;
+        }
+
+        if(!this.state.formError){
+            this.props.dispatch(auth_actions.authenticate(this.state.username, this.state.password));
+        }
+       
     }
 
     formValidation(){
         const globalState = getState()
         const error = {}
-        if(globalState.type === 'authentication_error'){
+        if(globalState.type.localeCompare('authentication_error'){
             
         } 
         
     }
 
-    renderTextField = ({id, label, placeholder, onChange, autoComplete, type, meta: {touched, error}}) => {
-        <TextField  id={id} 
-                    label={label}
-                    placeholder={placeholder} 
-                    onChange={onChange} 
-                    autoComplete={autoComplete}
-                    errorText={touch && error} 
-        />
-    }
+    // renderTextField = ({id, label, placeholder, onChange, autoComplete, type, meta: {touched, error}}) => {
+    //     <TextField  id={id} 
+    //                 label={label}
+    //                 placeholder={placeholder} 
+    //                 onChange={onChange} 
+    //                 autoComplete={autoComplete}
+    //                 errorText={touch && error} 
+    //     />
+    // }
 
     render () {
         const { classes } = this.props;
@@ -86,12 +99,11 @@ class Login extends Component {
                     <NavBar/>
                     <Paper style={{padding: "2%", width:"50%", margin:"auto", paddingLeft: "5%", paddingRight: "5%", marginTop: "5%"}}>
                         <Typography variant="headline" style={{marginBottom: "5%"}}>Login To StudyHour</Typography>
-                        {/* ERROR BOX HERE */ }
                         <Grid container>
                             <Grid item xs="12" className={classes.item}>
                                 <TextField
                                     id="standard-email"
-                                    label="User Name"
+                                    label="Email"
                                     placeholder="User Name"
                                     onChange={this.handleChangeUserName}
                                     autoComplete='off'
@@ -99,9 +111,10 @@ class Login extends Component {
                                     className={classes.textField}
                                     value={this.state.username}
                                     required
-                                    style={{paddingBottom: "10px"}}
+                                    style={{padding: "10px"}}
+                                    error={this.state.usernameInputError}
                                 />
-                                {submitted && !username && <FormHelperText id="component-helper-text">Some important helper text</FormHelperText>}
+                                {submitted && !username && <FormHelperText id="component-helper-text">Username is Required</FormHelperText>}
                             </Grid>
                             <Grid item xs="12" className={classes.item}>
                                 <TextField
@@ -114,17 +127,10 @@ class Login extends Component {
                                     className={classes.textField}
                                     value={this.state.password}
                                     required
-                                    style={{paddingBottom: "10px"}}
+                                    style={{padding: "10px"}}
+                                    error={this.state.userpasswordInputError}
                                 />
-                                {submitted && !password && <FormHelperText id="component-helper-text">Some important helper text</FormHelperText>}
-                            </Grid>
-                            <Grid item xs="12" style={{textAlign: "center", marginTop: 10}} className={classes.item}>
-                                <Button className={classes.button}
-                                        onClick={this.handleForgotPassword}
-                                        color="white"
-                                >
-                                    Forgot password?
-                                </Button>
+                                {submitted && !password && <FormHelperText id="component-helper-text">Password is Required</FormHelperText>}
                             </Grid>
                             <Grid item xs="12" style={{textAlign: "center", marginTop: 10}} className={classes.item}>
                                 <Button variant="contained"
