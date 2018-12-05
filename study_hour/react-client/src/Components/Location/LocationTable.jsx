bc5ed444683e99972db9f49b3899ab4779d8624a
xiaofanlin <xiaofanlinus@gmail.com>
2018-12-05 03:55:16 -0800

    you ask for it

--- study_hour/react-client/src/Components/Location/LocationTable.jsx
+++ study_hour/react-client/src/Components/Location/LocationTable.jsx
@@ -7,9 +7,9 @@ import {Link} from 'react-router-dom';
 import Button from "@material-ui/core/Button/Button";
 import * as addlocation_action from "../../actions/addlocation_action";
 import Typography from "@material-ui/core/Typography/Typography";
-import Paper from "@material-ui/core/Paper/Paper";
+import {Paper, Grid} from "@material-ui/core";
 import LocationAvgRating from "./LocationAvgRating";
-
+import Map from '../Map'
 function location_card(location) {
     const location_img = <img src={location.image_url} style={{borderRadius: '2%'}} width='500' height='350'/>;
     const no_img = <Typography variant='headline' color='error'>No Image Found</Typography>;
@@ -24,16 +24,13 @@ function location_card(location) {
             <Link to={'/Location/' + location.id}>
                 <div className="location-name">
                     <Typography variant="headline"
-                                style={{
-                                    fontSize: 50,
-                                    fontWeight: 500
-                                }}>{location.name}</Typography>
-                    <Typography variant="display4"
-                                style={{fontSize: 30}}>{location.address}</Typography>
+                                >{location.name}</Typography>
+                    <Typography variant='caption'
+                                >{location.address}</Typography>
                 </div>
             </Link>
-            <div className="location-rate">
-                <LocationAvgRating location_id={location.id}/>
+            <div>
+                <LocationAvgRating  location_id={location.id}/>
             </div>
         </div>
     </div>
@@ -53,14 +50,14 @@ class LocationTable extends Component {
 
     componentDidMount (){
         this.createTable();
-    }
-
-    createTable() {
         this.setState({locations: this.props.location.state.list});
         this.setState({favorite: this.props.location.state.favorite});
         this.setState({updated_locations: []});
         this.setState({updated_final_locations: []});
 
+    }
+
+    createTable() {
         let locations = this.props.location.state.list;
         Promise.all(locations.map(location => {
             axios({
@@ -108,22 +105,33 @@ class LocationTable extends Component {
         console.log('state', this.state);
         let locationTable;
         const add_button = (
-                    <div className="addLocation-button">
-                            <Button onClick={this.handleAddLocation} variant="contained" color="white">
+                    
+                            <Button  style={{float: 'left'}} onClick={this.handleAddLocation} variant="contained" color="white">
                                 Add Location
                             </Button>
-                    </div>
+                    
                     );
 
         locationTable = (
                 <Paper className='wallpaper'>
                     <NavBar />
-                    <Paper style={{padding: "2%", width:"80%", margin:"auto", marginTop: "3%", backgroundColor: 'rgba(66, 66, 66, .5)'}}>
+                    <Paper style={{padding: "1%", width:"90%", margin:"auto", marginTop: "3%"}}>
                     {!this.state.favorite ? add_button : ""}
-                    <div className='div-card-hold'>
+                    <Grid container spacing={20}>
+                    <Grid item md={7}>
+                    
+                    
                        {this.state.table}
+                    
+                    </Grid>
+                    <Grid item md={5}>
+
+                    <div style={{position: 'sticky', top: '5vh', marginTop: '3%'}} >
+                        <Map width="85%" />
                     </div>
-                    </Paper>
+                    </Grid>
+                    </Grid>
+                    </Paper>                    
                 </Paper>
             );
         
