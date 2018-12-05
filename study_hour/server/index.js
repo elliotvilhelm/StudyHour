b2c55ceace3730130d04d8f84ee56df3a798fb25
Elliot <elliot@pourmand.com>
2018-12-04 23:22:53 -0800

    stuff

--- study_hour/server/index.js
+++ study_hour/server/index.js
@@ -343,7 +343,7 @@ app.post('/api/location_liked', function (req, res, next) {
 });
 
 app.post('/api/upload/profile_image', function (req, res, next) {
-    pgClient.query('INSERT INTO profile_images(user_id, s3code) VALUES($1,$2,$3)',[req.body.user_id ,req.body.s3code], function (err, result) {
+    pgClient.query('INSERT INTO profile_images(user_id, s3code) VALUES($1,$2,$3)', [req.body.user_id ,req.body.s3code], function (err, result) {
         if (err) {
             return next(err)
         }
